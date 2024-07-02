07e69408a5273a1e7809d4951ae8987a1ec74285
Nathan Ribeiro <nathanribeiroo@me.com>
2024-07-02 19:49:23 -0300

    added soap

---
+++ soap/soap.go
@@ -0,0 +1,68 @@
+package soap
+
+import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func GetSefazConfig(xml string) (envelop string) {
+	var rxml = strings.ReplaceAll(xml, "&", "&amp;")
+	rxml = strings.ReplaceAll(rxml, "<", "&lt;")
+	rxml = strings.ReplaceAll(rxml, ">", "&gt;")
+	rxml = strings.ReplaceAll(rxml, "'", "&apos;")
+	rxml = strings.ReplaceAll(rxml, `"`, "&quot;")
+
+	envelop = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:sped="http://sped.fazenda.gov.br/">
+    <soapenv:Header />
+    <soapenv:Body>
+        <sped:ReceberLoteEventos>
+            <sped:loteEventos>
+                <Reinf xmlns="http://www.reinf.esocial.gov.br/schemas/envioLoteEventos/v1_05_01">
+                    <loteEventos>` + rxml + `</loteEventos>
+                </Reinf>
+            </sped:loteEventos>
+        </sped:ReceberLoteEventos>
+    </soapenv:Body>
+</soapenv:Envelope>`
+	return envelop
+}
+
+func SoapCall(cert tls.Certificate, baseURL, xml, soapAction string) string {
+
+	body := string(xml)
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				Renegotiation:      tls.RenegotiateOnceAsClient,
+				InsecureSkipVerify: true,
+				Certificates:       []tls.Certificate{cert},
+			},
+		},
+	}
+
+	req, _ := http.NewRequest("POST", baseURL, bytes.NewBufferString(body))
+	req.Header.Add("Content-Type", "text/xml")
+
+	if soapAction != "" {
+		req.Header.Add("SOAPAction", soapAction)
+	}
+
+	response, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+
+	content, _ := io.ReadAll(response.Body)
+
+	s := strings.TrimSpace(string(content))
+
+	return s
+}
