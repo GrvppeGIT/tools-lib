0c6f205f849bf8467b3fd6686be6e4de939ab3a5
Nathan Ribeiro <nathanribeiroo@me.com>
2024-07-04 14:57:56 -0300

    remove tag xmlns

--- signxml/types.go
+++ signxml/types.go
@@ -5,7 +5,7 @@ import "encoding/xml"
 type Signature struct {
 	XMLName        xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# Signature"`
 	SignedInfo     SignedInfo
-	SignatureValue string `xml:"http://www.w3.org/2000/09/xmldsig# SignatureValue"`
+	SignatureValue string `xml:"SignatureValue"`
 	KeyInfo        KeyInfo
 }
 
@@ -14,32 +14,32 @@ type Algorithm struct {
 }
 
 type SignedInfo struct {
-	XMLName                xml.Name  `xml:"http://www.w3.org/2000/09/xmldsig# SignedInfo"`
-	CanonicalizationMethod Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# CanonicalizationMethod"`
-	SignatureMethod        Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# SignatureMethod"`
+	XMLName                xml.Name  `xml:"SignedInfo"`
+	CanonicalizationMethod Algorithm `xml:"CanonicalizationMethod"`
+	SignatureMethod        Algorithm `xml:"SignatureMethod"`
 	Reference              Reference
 }
 
 type Reference struct {
-	XMLName      xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# Reference"`
+	XMLName      xml.Name `xml:"Reference"`
 	URI          string   `xml:",attr,omitempty"`
 	Transforms   Transforms
-	DigestMethod Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# DigestMethod"`
-	DigestValue  string    `xml:"http://www.w3.org/2000/09/xmldsig# DigestValue"`
+	DigestMethod Algorithm `xml:"DigestMethod"`
+	DigestValue  string    `xml:"DigestValue"`
 }
 
 type Transforms struct {
-	XMLName   xml.Name    `xml:"http://www.w3.org/2000/09/xmldsig# Transforms"`
-	Transform []Algorithm `xml:"http://www.w3.org/2000/09/xmldsig# Transform"`
+	XMLName   xml.Name    `xml:"Transforms"`
+	Transform []Algorithm `xml:"Transform"`
 }
 
 type KeyInfo struct {
-	XMLName  xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# KeyInfo"`
+	XMLName  xml.Name `xml:"KeyInfo"`
 	X509Data *X509Data
 	Children []interface{}
 }
 
 type X509Data struct {
-	XMLName         xml.Name `xml:"http://www.w3.org/2000/09/xmldsig# X509Data"`
-	X509Certificate string   `xml:"http://www.w3.org/2000/09/xmldsig# X509Certificate"`
+	XMLName         xml.Name `xml:"X509Data"`
+	X509Certificate string   `xml:"X509Certificate"`
 }
