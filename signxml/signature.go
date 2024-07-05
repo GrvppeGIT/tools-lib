45b74ebe2a3079b574082f0146e5ca3941e9411c
Nathan Ribeiro <nathanribeiroo@me.com>
2024-07-05 12:41:54 -0300

    added signature

---
+++ signxml/signature.go
@@ -0,0 +1,42 @@
+package signxml
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func SignXml(cnpj, xml, signLocation, action string) (string, error) {
+	postBody, _ := json.Marshal(map[string]interface{}{
+		"cnpj":         cnpj,
+		"xml":          xml,
+		"signTag":      signLocation,
+		"signLocation": signLocation,
+		"action":       action,
+		"reference":    true,
+	})
+
+	fmt.Println(string(postBody))
+
+	body := bytes.NewBuffer(postBody)
+
+	resp, err := http.Post(os.Getenv("SIGNATURE_BASE_URL")+"/reinf", "application/json", body)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	//Read the response body
+	responseBody, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(responseBody), err
+}
